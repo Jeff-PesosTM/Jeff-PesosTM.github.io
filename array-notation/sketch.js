592a9f60e5c83746057ce626727f3351d17eb807
Jeff-PesosTM <gradychovan@gmail.com>
2024-10-11 11:54:40 -0600

    started work on adding classes

--- array-notation/sketch.js
+++ array-notation/sketch.js
@@ -25,6 +25,41 @@ let mvmt = { // movement properties
   velocityFactor: 0.07 // velocity factor
 };
 
+class Walker {
+  constructor() {
+    this.x = width/2;
+    this.y = height/2;
+    this.color = "red";
+    this.speed = 5;
+  }
+
+  display() {
+    fill(this.color);
+    stroke(this.color);
+    ellipse(this.x, this.y, 2, 2);
+  }
+
+  move() {
+    let choice = random(100);
+    if (choice < 25) {
+    //up
+    this.y -= this.speed;
+    }
+    else if (choice < 50) {
+    //down
+    this.y += this.speed;
+    }
+    else if (choice < 75) {
+    //left
+    this.x -= this.speed;
+    }
+    else {
+    //right
+    this.x += this.speed;
+    }
+  }
+}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
