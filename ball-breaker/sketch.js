27a99a4d5046149ca930dd6aa5e972e68d4ad662
Jeff-PesosTM <gradychovan@gmail.com>
2024-10-16 14:12:25 -0600

    fixed so many thing and added brick collisions

--- ball-breaker/sketch.js
+++ ball-breaker/sketch.js
@@ -2,29 +2,38 @@
 // array and object notation project
 // 10/15/2024
 
-let ballX = 200;  
-let ballY = 200; 
+let ballX;  
+let ballY; 
 let dx = 5;
-let dy = 3;
+let dy = -3;
 let ballSize = 30;
+
 let obstacleArray = [];
+
 let brickAmount = 7;
 let brickSize = 40;
 let spacing = 100;
 
+let sideBuffer = 50;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < brickAmount; i++) {
-    spawnBrick(i);
+  ballY = windowHeight-(40 + 10);
+  ballX = windowWidth/2;
+  for(let x = sideBuffer/2; x <= windowWidth - sideBuffer/2; x += spacing){
+    let aBrick = spawnBrick(x, 0);
+    obstacleArray.push(aBrick);
   }
+  console.log(obstacleArray);
 }
 
 function draw() {
   background(220);
-  displayObstacles();
   player();
   ballLogic();
-  checkCollision()
+  checkCollision();
+  displayObstacles();
 }
 
 function player() {
@@ -33,48 +42,50 @@ function player() {
 
 function ballLogic(){
   ellipse(ballX, ballY, ballSize, ballSize); // ball
- 
-  if(ballX >= width-20 || ballX === 20){
-    dx = -dx;
+  if (ballX >= width - ballSize/2 || ballX <= 0 + ballSize/2){
+    dx *= -1;
   }
-  if(ballY >= height-20|| ballY === 20) {
-    dy = -dy;
+  if (ballY >= height - ballSize/2 || ballY <= 0 + ballSize/2) {
+    dy *= -1;
   }
   ballX += dx;
   ballY += dy;
 }
 
 
-// death logic
+// brick death logic
+
 function checkCollision() {
   for (let brick of obstacleArray){
     if (obstacleHit(ballX, ballY, brick)) {
-      bubbleArray.splice(obstacleArray.indexOf(bubble), 1);
+      obstacleArray.splice(obstacleArray.indexOf(brick), 1);
+      dy *= -1;
     }
   }
 }
 
-function obstacleHit(x,y, theBrick) {
-  let distanceAway = dist(x, y, theBrick.x, theBrick.y);
-  return distanceAway < ballSize; 
+
+function obstacleHit(x,y, theBrick) { // used in collision function
+  let distanceAway = dist(x, y, theBrick.x + brickSize*2, theBrick.y + brickSize);
+  return distanceAway <= ballSize; 
 }
+
+
 
 // obstacle logic
-function spawnBrick(i){
-  let someBrick;
-  for (let x = 0; x < windowWidth; x+= TILE_SIZE) {
-    for(let y = 0; y < windowHeight; y += TILE_SIZE){
-      let aTile = spawnTile(x,y);
-      console.log(dist(x, y, mouseX, mouseY)/100);
-      tileArray.push(aTile);
-    }
-  }
 
-  obstacleArray.push(someBrick);
+// spawning obstacle function used in setup
+function spawnBrick(x,y){
+  let brick = {
+    x: x,
+    y: y,
+  };
+  return brick;
 }
 
+//displays the bricks
 function displayObstacles() {
-  for (let brick of obstacleArray){
-    rect(brick.x, brick.y, brickSize * 2, brickSize);
+  for (let aBrick of obstacleArray){
+    rect(aBrick.x, aBrick.y, brickSize * 2, brickSize);
   }
 }
